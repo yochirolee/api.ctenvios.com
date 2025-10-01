6dbd38c6a97f28dfa89f4bfac2ed1ce322d3330d
Yochiro Lee Cruz <yosho@Yochiros-MacBook-Pro.local>
2025-10-01 14:27:58 -0400

    shipping rates 2

--- src/routes/roles.routes.ts
+++ src/routes/roles.routes.ts
@@ -4,20 +4,17 @@ import { authMiddleware } from "../middlewares/auth-midleware";
 import z from "zod";
 const router = Router();
 
-
-
-
 router.get("/", authMiddleware, async (req: any, res: any) => {
-	const roles = Object.values(Roles);
-	//how to get equal or below roles
-	const user = req?.user;
-	const permited_roles = [Roles.ROOT, Roles.ADMINISTRATOR];
-	if(!permited_roles.includes(user.role)) {
-		return res.status(403).json({ message: "You are not authorized to get roles" });
-	}
-	//convert to array of objects with id and name
-	const rolesWithId = roles.map((role) => ({ id: role, name: role }));
-	res.status(200).json(rolesWithId);
+   const roles = Object.values(Roles);
+   //how to get equal or below roles
+   const user = req?.user;
+   const permited_roles = [Roles.ROOT, Roles.ADMINISTRATOR];
+   if (!permited_roles.includes(user.role)) {
+      return res.status(403).json({ message: "You are not authorized to get roles" });
+   }
+   //convert to array of objects with id and name
+   const rolesWithId = roles.map((role) => ({ id: role, name: role }));
+   res.status(200).json(rolesWithId);
 });
 
 export default router;
